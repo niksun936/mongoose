45418db8e77585d01c3d6c46b9385d39d534ec9c
jfsimon <jfsimon@startmail.com>
2023-02-17 10:28:13 +0000

    cf

--- examples/nxp/nxp-evkmimxrt1020-tcpip-baremetal/hal.c
+++ examples/nxp/nxp-evkmimxrt1020-tcpip-baremetal/hal.c
@@ -1,11 +1,11 @@
 
 
+#include "hal.h"
 #include <stdarg.h>
 #include <stdbool.h>
-#include <string.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "hal.h"
+#include <string.h>
 
 #define ASSERT(expr)                                            \
   do {                                                          \
@@ -19,25 +19,24 @@
 #define SETBITS(R, CLEARMASK, SETMASK) (R) = ((R) & ~(CLEARMASK)) | (SETMASK)
 
 void set_user_led(int i) {
-	(void) i;
-/*
-	if (!i)
-		GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, 1U); // Led OFF
-	else
-		GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, 0U); // Led ON
-*/
+  (void) i;
+  /*
+          if (!i)
+                  GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN,
+     1U); // Led OFF else GPIO_PinWrite(BOARD_USER_LED_GPIO,
+     BOARD_USER_LED_GPIO_PIN, 0U); // Led ON
+  */
 }
 
-void toggle_user_led()
-{
-	static bool led_is_on = 0;
-	if (!led_is_on) {
-		led_is_on = 1;
-		set_user_led(1);
-	} else {
-		led_is_on = 0;
-		set_user_led(0);
-	}
+void toggle_user_led() {
+  static bool led_is_on = 0;
+  if (!led_is_on) {
+    led_is_on = 1;
+    set_user_led(1);
+  } else {
+    led_is_on = 0;
+    set_user_led(0);
+  }
 }
 
 /*
@@ -47,24 +46,25 @@ void toggle_user_led()
  */
 
 void board_init(void) {
-    // SDK Version
-/*
-    // Hardware Initialization.
-    const gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
+  // SDK Version
+  /*
+      // Hardware Initialization.
+      const gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0,
+     kGPIO_NoIntmode};
 
-    // MPU and peripheral setup
-    BOARD_ConfigMPU();
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitDebugConsole();
-    clock_init();
+      // MPU and peripheral setup
+      BOARD_ConfigMPU();
+      BOARD_InitBootPins();
+      BOARD_InitBootClocks();
+      BOARD_InitDebugConsole();
+      clock_init();
 
-    // User LED
-    GPIO_PinInit(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, &gpio_config);
-    GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, 1U);
+      // User LED
+      GPIO_PinInit(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, &gpio_config);
+      GPIO_PinWrite(BOARD_USER_LED_GPIO, BOARD_USER_LED_GPIO_PIN, 1U);
 
-    ASSERT(!SysTick_Config(SystemCoreClock / 1000U)); // 1ms
-*/
+      ASSERT(!SysTick_Config(SystemCoreClock / 1000U)); // 1ms
+  */
 }
 
 /*
@@ -73,42 +73,43 @@ void board_init(void) {
  * No enet peripheral access or DMA enable before clocks/gates are configured.
  */
 
-void clock_init(void)
-{
-    // PHY: clk 50 MHz
-/*
-    const clock_enet_pll_config_t config = {.enableClkOutput = true, .enableClkOutput500M = true, .loopDivider = 1};
-    CLOCK_InitEnetPll(&config);
-    IOMUXC_EnableMode(IOMUXC_GPR, kIOMUXC_GPR_ENET1TxClkOutputDir, true);
-*/
+void clock_init(void) {
+  // PHY: clk 50 MHz
+  /*
+      const clock_enet_pll_config_t config = {.enableClkOutput = true,
+     .enableClkOutput500M = true, .loopDivider = 1}; CLOCK_InitEnetPll(&config);
+      IOMUXC_EnableMode(IOMUXC_GPR, kIOMUXC_GPR_ENET1TxClkOutputDir, true);
+  */
 }
 
 // for Clocks, IOs and ENET
 void peripheral_enet_init(void) {
-/*
-    // SDK Version
-    const gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0, kGPIO_NoIntmode};
-    // GPIO Setup
-    GPIO_PinInit(GPIO1, 4, &gpio_config);  // PHY: #RST
-    GPIO_PinInit(GPIO1, 22, &gpio_config); // PHY: INTRP
-    // Pull up the ENET_INT before RESET.
-    GPIO_PinWrite(GPIO1, 22, 1); // PHY: interrupt
-    GPIO_PinWrite(GPIO1, 4, 0);  // PHY: reset
-    SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CpuClk));
-    GPIO_PinWrite(GPIO1, 4, 1);  // PHY: active
+  /*
+      // SDK Version
+      const gpio_pin_config_t gpio_config = {kGPIO_DigitalOutput, 0,
+     kGPIO_NoIntmode};
+      // GPIO Setup
+      GPIO_PinInit(GPIO1, 4, &gpio_config);  // PHY: #RST
+      GPIO_PinInit(GPIO1, 22, &gpio_config); // PHY: INTRP
+      // Pull up the ENET_INT before RESET.
+      GPIO_PinWrite(GPIO1, 22, 1); // PHY: interrupt
+      GPIO_PinWrite(GPIO1, 4, 0);  // PHY: reset
+      SDK_DelayAtLeastUs(1000000, CLOCK_GetFreq(kCLOCK_CpuClk));
+      GPIO_PinWrite(GPIO1, 4, 1);  // PHY: active
 
-	static mdio_handle_t mdioHandle = {.ops = &enet_ops};
-	static phy_handle_t phyHandle   = {.phyAddr = 0, .mdioHandle = &mdioHandle, .ops = &phyksz8081_ops};
+          static mdio_handle_t mdioHandle = {.ops = &enet_ops};
+          static phy_handle_t phyHandle   = {.phyAddr = 0, .mdioHandle =
+     &mdioHandle, .ops = &phyksz8081_ops};
 
-	phy_config_t phyConfig = {0};
-    phyConfig.phyAddr               = 0x02U; // PHY address
-    phyConfig.autoNeg               = true;
-    mdioHandle.resource.base        = ENET;
-    mdioHandle.resource.csrClock_Hz = CLOCK_GetFreq(kCLOCK_IpgClk);
+          phy_config_t phyConfig = {0};
+      phyConfig.phyAddr               = 0x02U; // PHY address
+      phyConfig.autoNeg               = true;
+      mdioHandle.resource.base        = ENET;
+      mdioHandle.resource.csrClock_Hz = CLOCK_GetFreq(kCLOCK_IpgClk);
 
-	PHY_Init(&phyHandle, &phyConfig);
-	CLOCK_EnableClock(kCLOCK_Enet);
-*/
+          PHY_Init(&phyHandle, &phyConfig);
+          CLOCK_EnableClock(kCLOCK_Enet);
+  */
 }
 
 int PRINTF(const char *fmt_s, ...) {
