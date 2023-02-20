f49b16fe6916ad893643d45981b6967de2444b3d
jfsimon <jfsimon@startmail.com>
2023-02-20 10:48:55 +0000

    Add file, update.

---
+++ examples/nxp/nxp-evkmimxrt1020-tcpip-baremetal/sysinit.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Cesanta Software Limited
+// All rights reserved
+//
+// This file contains essentials required by the CMSIS:
+// uint32_t SystemCoreClock - holds the system core clock value
+// SystemInit() - initialises the system, e.g. sets up clocks
+
+#include "hal.h"
+
+void SystemInit(void) {
+  // Todo
+}
